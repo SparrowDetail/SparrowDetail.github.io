f90350f2a204cf7a34cf2596d03b626d9e8a49eb
StevenS <sshort95@gmail.com>
2024-07-15 21:00:52 -0400

    Initial commit

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+function open_mobile_nav() {
+    const mobile_nav = document.querySelector(".mobile-nav");
+    mobile_nav.style.display = 'flex';
+}
+
+function close_mobile_nav() {
+    const mobile_nav = document.querySelector(".mobile-nav");
+    mobile_nav.style.display = 'none';
+}
