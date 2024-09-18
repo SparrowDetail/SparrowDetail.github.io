eaee2f1c3aa5d2f3e2aa42f7f2a11aa13c6f13c7
StevenS <sshort95@gmail.com>
2024-09-18 14:52:16 -0400

    setScrollVar function doc comment

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,7 @@
+
+
+/**Calculates scroll variable as a ratio of html scroll distance to client
+ * height, i.e. --scroll of 1 will be a distance of 100vh */
 const setScrollVar = () => {
     const page = document.documentElement;
     const scrollAmount = page.scrollTop / page.clientHeight;
@@ -8,3 +12,4 @@ setScrollVar();
 
 window.addEventListener("scroll", setScrollVar);
 window.addEventListener("resize", setScrollVar);
+
